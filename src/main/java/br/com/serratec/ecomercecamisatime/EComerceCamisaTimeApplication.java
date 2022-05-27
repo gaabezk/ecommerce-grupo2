518a2dac428ce315807ec39dffd1a7af92ed0285
Gabriel Fernandes <gaabezk@gmail.com>
2022-05-27 16:30:13 -0300

    initial commit

---
+++ src/main/java/br/com/serratec/ecomercecamisatime/EComerceCamisaTimeApplication.java
@@ -0,0 +1,13 @@
+package br.com.serratec.ecomercecamisatime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EComerceCamisaTimeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EComerceCamisaTimeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/serratec/ecomercecamisatime/EComerceCamisaTimeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.serratec.ecomercecamisatime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EComerceCamisaTimeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
